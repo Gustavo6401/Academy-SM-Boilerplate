4a23ed645858f770e83f4b2f6cf02799dfb52ce7
Gustavo6401 <gsoey2019123@gmail.com>
2023-09-30 13:44:18 -0300

    Primeiro Boilerplate em React JS

---
+++ Index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+import './Index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <h1>AE PORRA!!!</h1>
+);
